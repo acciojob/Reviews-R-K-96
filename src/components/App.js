953ace3aec7ca33f2c3c70bb14742acd5167be04
R-K-96 <kashmirehrithik@gmail.com>
2023-05-23 22:47:00 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,85 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react';
 
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      text:
+        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviews[index];
+
+  const nextReview = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  };
+
+  const prevReview = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + reviews.length) % reviews.length);
+  };
+
+  const randomReview = () => {
+    const randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  };
+
+  return (
+    <div>
+      <h2 id="review-heading">Our Reviews</h2>
+      <div className="review">
+        <div className="person-img">
+          <img src={image} alt={name} />
+        </div>
+        <h4>{name}</h4>
+        <p>{job}</p>
+        <p>{text}</p>
+      </div>
+      <div className="btn-container">
+        <button className="prev-btn" onClick={prevReview}>
+          Previous
+        </button>
+        <button className="next-btn" onClick={nextReview}>
+          Next
+        </button>
+        <button className="random-btn" onClick={randomReview}>
+          Surprise Me
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
